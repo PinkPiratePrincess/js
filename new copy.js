7a365f684539b5dc0e0a1d6249e8a52eb0cd50f6
PinkPiratePrincess <140835778+PinkPiratePrincess@users.noreply.github.com>
2024-01-27 18:00:27 +0300

    2

--- new copy.js
+++ new copy.js
@@ -1,27 +1,29 @@
-let words = ["котомото"]; 
-   
-   let word = words[Math.floor(Math.random() * words.length)]; 
-   
-   let answerArray = []; 
-   for (let i = 0; i < word.length; i++){
-       answerArray[i] = "_";   
-   }
-   
-   let remainingLetters = word.length; 
+let words = ["котомото"];
 
-while (remainingLetters > 0) { 
+let word = words[Math.floor(Math.random() * words.length)];
 
-    alert(answerArray.join(" "));  
-        
-    let guess = prompt("Угадай букву, или нажми 'Отмена' если сдаёшься")
-        if (guess === null){break;}  
-        else if (guess.length !== 1) {alert("Нужно ввести только одну букву");}
-        else {for (let j = 0; j < word.length; j++){  
-            if (word[j] === guess){ 
-                answerArray[j] = guess;  
-                remainingLetters--; 
-        }
+let answerArray = [];
+for (let i = 0; i < word.length; i++) {
+  answerArray[i] = "_";
+}
+
+let remainingLetters = word.length;
+
+while (remainingLetters > 0) {
+  alert(answerArray.join(" "));
+  let guess = prompt("Угадай букву, или нажми 'Отмена' если сдаёшься").toLowerCase();
+  if (guess === null) {
+    break;
+  } else if (guess.length !== 1) {
+    alert("Нужно ввести только одну букву");
+  } else {
+    for (let j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+        answerArray[j] = guess;
+        remainingLetters--;
+      }
     }
-         }}
-   alert(answerArray.join(" "));
+  }
+}
+alert(answerArray.join(" "));
 alert("Отлично! Было загадано слово " + word);
